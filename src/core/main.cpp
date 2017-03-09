1c7472611a8738d7ff56ba74c2a1247757a2f2c0
mike31 <michael.rozantsev@gmail.com>
2017-03-09 17:29:33 +0200

    BC commit b4bbad1. Ignore getheaders requests when not synced.

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -5726,6 +5726,10 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 
         LOCK(cs_main);
 
+        if (IsInitialBlockDownload())
+            return true;
+
+          
         CBlockIndex* pindex = NULL;
         if (locator.IsNull())
         {
