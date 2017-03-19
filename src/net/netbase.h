09005d3369415d89997c182ff9e77f865f080ba2
mike31 <michael.rozantsev@gmail.com>
2017-03-19 09:45:01 +0200

    BC commit 182686c. Fix two problems in CSubnet parsing

--- src/net/netbase.h
+++ src/net/netbase.h
@@ -101,6 +101,8 @@ class CNetAddr
         inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion) {
             READWRITE(FLATDATA(ip));
         }
+        
+        friend class CSubNet;       
 };
 
 class CSubNet
