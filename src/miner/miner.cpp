5976f05f5908557aa7895f5706541595c36524e3
mike31 <michael.rozantsev@gmail.com>
2017-05-10 17:45:29 +0300

    Skipping block logging if not needed

--- src/miner/miner.cpp
+++ src/miner/miner.cpp
@@ -793,8 +793,8 @@ CBlockTemplate* CreateNewBlockWithDefaultKey(CWallet *pwallet,int *canMine,const
 
 bool ProcessBlockFound(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey)
 {
-    LogPrint("mchnminor","%s\n", pblock->ToString());
-    LogPrint("mcminer","mchn-miner: generated %s\n", FormatMoney(pblock->vtx[0].vout[0].nValue));
+    if(fDebug)LogPrint("mchnminor","%s\n", pblock->ToString());
+    if(fDebug)LogPrint("mcminer","mchn-miner: generated %s\n", FormatMoney(pblock->vtx[0].vout[0].nValue));
 
     // Found a solution
     {
