08c92cce81d48e364675893f6bebcff4b275ed31
mike31 <michael.rozantsev@gmail.com>
2017-10-24 12:50:28 +0300

    Skipping send permission check in signrawtransaction

--- src/rpc/rpcrawtransaction.cpp
+++ src/rpc/rpcrawtransaction.cpp
@@ -1927,7 +1927,7 @@ Value signrawtransaction(const Array& params, bool fHelp)
         BOOST_FOREACH(const CMutableTransaction& txv, txVariants) {
             txin.scriptSig = CombineSignatures(prevPubKey, mergedTx, i, txin.scriptSig, txv.vin[i].scriptSig);
         }
-        if (!VerifyScript(txin.scriptSig, prevPubKey, STANDARD_SCRIPT_VERIFY_FLAGS, MutableTransactionSignatureChecker(&mergedTx, i)))
+        if (!VerifyScript(txin.scriptSig, prevPubKey, STANDARD_SCRIPT_VERIFY_FLAGS | SCRIPT_VERIFY_SKIP_SEND_PERMISSION_CHECK, MutableTransactionSignatureChecker(&mergedTx, i)))
             fComplete = false;
     }
 
