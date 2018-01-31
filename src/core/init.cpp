8812024f35c73ef7a11fbd26dce922795142a4bc
Mayank <mayankvijh89@gmail.com>
2018-01-31 12:42:58 +0530

    testing parameter set

--- src/core/init.cpp
+++ src/core/init.cpp
@@ -1284,6 +1284,8 @@ bool AppInit2(boost::thread_group& threadGroup,int OutputPipe)
 
                 if(mc_gState->m_NetworkParams->m_Status != MC_PRM_STATUS_VALID)
                 {
+                    sprintf(bufOutput,mc_gState->m_NetworkParams->m_Status);
+                    bytes_written=write(OutputPipe,bufOutput,strlen(bufOutput));
                     return InitError(_("Invalid parameter set"));
                 }                        
             }
