23b2e5eb0cb952ae4ed4cbb4a6168b26190550f9
mike31 <michael.rozantsev@gmail.com>
2018-06-10 13:40:33 +0300

    onchain/offchain instead of onchainonly/offchainonly

--- src/rpc/rpcrawdata.cpp
+++ src/rpc/rpcrawdata.cpp
@@ -741,8 +741,8 @@ bool RawDataParseRestrictParameter(const Value& param,uint32_t *restrict,uint32_
             {
                 match=0;
                 if(( (ptr-start) ==  5) && (memcmp(start,"write",    ptr-start) == 0) ){match = 1; *permissions |= MC_PTP_WRITE ;}
-                if(( (ptr-start) == 11) && (memcmp(start,"onchainonly",  ptr-start) == 0) ){match = 1; *restrict |= MC_ENT_ENTITY_RESTRICTION_ONCHAIN;}
-                if(( (ptr-start) == 12) && (memcmp(start,"offchainonly", ptr-start) == 0) ){match = 1; *restrict |= MC_ENT_ENTITY_RESTRICTION_OFFCHAIN;}
+                if(( (ptr-start) ==  7) && (memcmp(start,"onchain",  ptr-start) == 0) ){match = 1; *restrict |= MC_ENT_ENTITY_RESTRICTION_ONCHAIN;}
+                if(( (ptr-start) ==  8) && (memcmp(start,"offchain", ptr-start) == 0) ){match = 1; *restrict |= MC_ENT_ENTITY_RESTRICTION_OFFCHAIN;}
                 
                 if(match == 0)
                 {
