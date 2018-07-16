47cebfb23b6bf40e2706fdaef98d02f052119caa
mike31 <michael.rozantsev@gmail.com>
2018-07-16 14:38:28 +0300

    Fixed txcount in getwalletinfo

--- src/rpc/rpcwallet.cpp
+++ src/rpc/rpcwallet.cpp
@@ -2169,7 +2169,7 @@ Value getwalletinfo(const Array& params, bool fHelp)
     obj.push_back(Pair("walletdbversion", mc_gState->GetWalletDBVersion()));                
     if(mc_gState->m_WalletMode & MC_WMD_ADDRESS_TXS) 
     {
-        obj.push_back(Pair("txcount",       (int)pwalletTxsMain->m_Database->m_DBStat.m_Count+(int)pwalletTxsMain->m_UnconfirmedSends.size()));        
+        obj.push_back(Pair("txcount",(int)pwalletTxsMain->m_Database->m_DBStat.m_Count+pwalletTxsMain->m_Database->m_RawMemPools[0]->GetCount()));        
     }
     else
     {
