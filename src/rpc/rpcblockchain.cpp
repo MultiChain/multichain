0cb59896aade755a1722acf46c95f1365a5e5611
mike31 <michael.rozantsev@gmail.com>
2018-11-05 14:39:38 +0200

    Fixed getfiltertxinput when all inputs are spent in the same block

--- src/rpc/rpcblockchain.cpp
+++ src/rpc/rpcblockchain.cpp
@@ -592,7 +592,10 @@ Value gettxout(const Array& params, bool fHelp)
         CCoinsViewMemPool view(pcoinsTip, mempool);
         if (!view.GetCoins(hash, coins))
             return Value::null;
-        mempool.pruneSpent(hash, coins); // TODO: this should be done by the CCoinsViewMemPool
+        if(pMultiChainFilterEngine->m_TxID == 0)                                // In filter we already checked this input exists, but mempool is dirty
+        {
+            mempool.pruneSpent(hash, coins); // TODO: this should be done by the CCoinsViewMemPool
+        }
     } else {
         if (!pcoinsTip->GetCoins(hash, coins))
             return Value::null;
