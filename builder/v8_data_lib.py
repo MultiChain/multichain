05088259bb81f754bacdd43d8c1dfe4a1c3d129a
Zvi Tarem <Zvi.Tarem@gmail.com>
2019-01-06 14:02:26 +0200

    Added --platform to builder/v8_data_lib.py

--- builder/v8_data_lib.py
+++ builder/v8_data_lib.py
@@ -10,34 +10,36 @@ from pathlib2 import Path
 logger = logging.getLogger(str(Path(__file__).stem))
 
 
-def get_bin_type():
-    if sys.platform == "darwin":
-        bin_type = "mach-o-x86-64"
-    elif sys.platform == "win32":
-        bin_type = "pe-x86-64"
+def get_bin_type(platform):
+    if platform == "darwin":
+        bin_type = ("mach-o-x86-64", ".o", "lib{}.a")
+    elif platform == "win32":
+        bin_type = ("pe-x86-64", ".obj", "{}.lib")
     else:
-        bin_type = "elf64-x86-64"
+        bin_type = ("elf64-x86-64", "{}.o", "lib{}.a")
     return bin_type
 
 
-def process_bin_file(filepath):
+def process_bin_file(filepath, platform):
     logger.debug("process_bin_file(filepath={})".format(filepath))
     assert filepath.exists()
-    obj_name = filepath.with_suffix(".o").name
+    arch, obj_suffix, _lib_pattern = get_bin_type(platform)
+    obj_name = filepath.with_suffix(obj_suffix).name
     obj_path = Path("obj") / obj_name
-    cmd = ["objcopy", "-B", "i386", "-I", "binary", "-O", get_bin_type(), filepath.name, str(obj_path)]
+    cmd = ["objcopy", "-B", "i386", "-I", "binary", "-O", arch, filepath.name, str(obj_path)]
     logger.info(' '.join(cmd))
     call(cmd)
     return obj_name
 
 
-def process_bin_files():
+def process_bin_files(platform):
     cwd = Path.cwd()
     obj_names = []
     for f in chain(cwd.glob("*.bin"), cwd.glob("*.dat")):
-        obj_names.append(process_bin_file(f))
+        obj_names.append(process_bin_file(f, platform))
     os.chdir("obj")
-    cmd = ["ar", "rvs", "libv8_data.a"]
+    _arch, _obj_suffix, lib_pattern = get_bin_type(platform)
+    cmd = ["ar", "rvs", lib_pattern.format("v8_data")]
     cmd += obj_names
     logger.info(' '.join(cmd))
     call(cmd)
@@ -50,6 +52,8 @@ def get_options():
     parser.add_argument("-p", "--path", metavar="PATH", type=Path, default=default_path.resolve(),
                         help="path to the V8 build area (default: %(default)s)")
     parser.add_argument("-b", "--binutils", metavar="PATH", type=Path, help="path to binutils")
+    parser.add_argument("-o", "--platform", default=sys.platform,
+                        help="override platform definition (default: %(default)s)")
 
     options = parser.parse_args()
 
@@ -58,6 +62,7 @@ def get_options():
 
     logger.info("{} - {}".format(logger.name, parser.description))
     logger.info("  Path:     {}".format(options.path))
+    logger.info("  Platform: {}".format(options.platform))
     if options.binutils:
         logger.info("  Binutils: {}".format(options.binutils))
 
@@ -70,7 +75,7 @@ def main():
     if options.binutils:
         os.environ["PATH"] = os.pathsep.join(str(options.binutils / "bin"), os.environ["PATH"])
     os.chdir(str(options.path))
-    process_bin_files()
+    process_bin_files(options.platform)
     return 0
 
 
