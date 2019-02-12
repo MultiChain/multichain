2e0b386f2f8de3454da3efa36c0fe896079544db
mike31 <michael.rozantsev@gmail.com>
2019-02-12 08:17:35 +0200

    Fixed for Mac - block pkh calculation error

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -1110,8 +1110,9 @@ bool TransactionSignatureChecker::CheckSig(const vector<unsigned char>& vchSigIn
         if(mc_gState->m_NetworkParams->IsProtocolMultichain())
         {
             const unsigned char *pubkey_hash=(unsigned char *)Hash160(vchPubKey.begin(),vchPubKey.end()).begin();
-
-            if(mc_gState->m_Permissions->CanSend(NULL,pubkey_hash))
+//            if(mc_gState->m_Permissions->CanSend(NULL,pubkey_hash))
+            uint160 hash=Hash160(vchPubKey.begin(),vchPubKey.end());
+            if(mc_gState->m_Permissions->CanSend(NULL,&hash))
             {
                 CheckSendPermission=false;                    
             }                                    
