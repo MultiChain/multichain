5b820680f9fbee8f846abecde5e712f3e5196eb9
mike31 <michael.rozantsev@gmail.com>
2020-10-11 11:49:41 +0300

    Fixed checking non-library approvals

--- src/protocol/multichaintx.cpp
+++ src/protocol/multichaintx.cpp
@@ -1354,7 +1354,7 @@ bool MultiChainTransaction_CheckIfLibraryApproval(unsigned char* ptr,
     mc_EntityDetails entity;
     if(mc_gState->m_Assets->FindEntityByShortTxID(&entity,ptr))
     {
-        if(entity->GetEntityType() == MC_ENT_TYPE_LIBRARY)
+        if(entity.GetEntityType() == MC_ENT_TYPE_LIBRARY)
         {
             details->SetAffectedLibrary((unsigned char*)entity.GetTxID()+MC_AST_SHORT_TXID_OFFSET);       
             return true;
