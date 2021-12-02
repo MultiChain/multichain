8e8257093fca96610ac7fa70b3db8dbf059db563
mike31 <michael.rozantsev@gmail.com>
2021-12-02 14:21:36 +0200

    Fixed multichain-cli for newer versions of event

--- src/multichain/multichain-cli.cpp
+++ src/multichain/multichain-cli.cpp
@@ -276,6 +276,13 @@ static void http_request_done(struct evhttp_request *req, void *ctx)
     }
 }
 
+#if LIBEVENT_VERSION_NUMBER >= 0x02010300
+static void http_error_cb(enum evhttp_request_error err, void *ctx)
+{
+    HTTPReply *reply = static_cast<HTTPReply*>(ctx);
+    reply->error = err;
+}
+#endif
 
 Object CallRPC(const string& strMethod, const Array& params)
 {
