ec9ee4f1ebaba960be7da6d94995de40b5f4f05f
mike31 <michael.rozantsev@gmail.com>
2022-08-15 14:04:07 +0300

    Block map object

---
+++ src/chain/blockmap.cpp
@@ -0,0 +1,72 @@
+// Copyright (c) 2009-2010 Satoshi Nakamoto
+// Copyright (c) 2009-2014 The Bitcoin developers
+// Original code was distributed under the MIT software license.
+// Copyright (c) 2014-2019 Coin Sciences Ltd
+// MultiChain code distributed under the GPLv3 license, see COPYING file.
+
+#include "chain/blockmap.h"
+#include "core/main.h"
+
+using namespace std;
+
+int nBlockIndexCacheSize = DEFAULT_MAX_BLOCK_INDEX_CACHE_SIZE;
+
+CBlockIndex* CCachedBlockIndex::getpindex()
+{
+    return m_PIndex;
+}
+
+void CCachedBlockIndex::setpindex(CBlockIndex* pindex)
+{
+    m_Hash = 0;
+    if(pindex)
+    {
+        m_Hash = pindex->GetBlockHash();
+    }
+    m_PIndex = pindex;
+}
+
+CBlockIndex *CCachedBlockIndex::operator=(CBlockIndex* pindex)
+{
+    setpindex(pindex);
+    return m_PIndex;
+}
+
+
+CBlockIndex *CBlockMap::operator[](uint256 hash) {
+    return mapBlockIndex[hash];
+}
+
+size_t CBlockMap::count(uint256 hash) {
+    return mapBlockIndex.count(hash);
+}
+
+BlockMap::iterator CBlockMap::find(uint256 hash) {
+    return mapBlockIndex.find(hash);
+}
+
+BlockMap::iterator CBlockMap::begin() {
+    return mapBlockIndex.begin();
+}
+
+BlockMap::iterator CBlockMap::end() {
+    return mapBlockIndex.end();
+}
+
+std::pair<BlockMap::iterator, bool> CBlockMap::insert(std::pair<uint256, CBlockIndex*> x) {
+    return mapBlockIndex.insert(x);
+}
+
+bool CBlockMap::empty() {
+    return mapBlockIndex.empty();
+}
+
+
+/* Returns number of cached elements, not full size of the map */
+size_t CBlockMap::size() {
+    return mapBlockIndex.size();
+}
+
+void CBlockMap::clear() {
+    return mapBlockIndex.clear();
+}
---
+++ src/chain/blockmap.h
@@ -0,0 +1,56 @@
+// Copyright (c) 2014-2022 Coin Sciences Ltd
+// MultiChain code distributed under the GPLv3 license, see COPYING file.
+
+#ifndef MULTICHAIN_BLOCKMAP_H
+#define MULTICHAIN_BLOCKMAP_H
+
+#include "structs/uint256.h"
+#include <boost/unordered_map.hpp>
+
+class CBlockIndex;
+
+struct BlockHasher
+{
+    size_t operator()(const uint256& hash) const { return hash.GetLow64(); }
+};
+
+extern int nBlockIndexCacheSize;
+
+typedef boost::unordered_map<uint256, CBlockIndex*, BlockHasher> BlockMap;
+
+class CCachedBlockIndex{
+private:
+    uint256 m_Hash;
+    CBlockIndex *m_PIndex;
+public:
+    CCachedBlockIndex(CBlockIndex* pindex)
+    {
+        setpindex(pindex);
+    }
+    
+    CBlockIndex* getpindex();     
+    void setpindex(CBlockIndex* pindex);     
+    CBlockIndex *operator=(CBlockIndex* pindex);
+};
+
+class CBlockMap {
+private:
+    
+    BlockMap mapBlockIndex;
+
+public:
+    
+    CBlockIndex *operator[](uint256 hash);
+    size_t count(uint256 hash);    
+    BlockMap::iterator find(uint256 hash);
+    BlockMap::iterator begin();
+    BlockMap::iterator end();
+    std::pair<BlockMap::iterator, bool> insert(std::pair<uint256, CBlockIndex*> x);
+    bool empty();
+    size_t size();
+    void clear();
+};
+
+
+#endif /* MULTICHAIN_BLOCKMAP_H */
+
